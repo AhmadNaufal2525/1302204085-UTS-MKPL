51526652533460ed2cce305e7a34ab27b0574fa2
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-04-14 17:46:51 +0700

    menambahkan setter grade

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -10,21 +10,22 @@ public class Employee {
 		GRADE2,
 		GRADE3
 	}
+
 	private int yearJoined;
 	private int monthJoined;
 	private int monthWorkingInYear;
 	private Grade grade;
 	private boolean isForeigner;
-	
+
 	private int monthlySalary;
 	private int otherMonthlyIncome;
 	private int annualDeductible;
-	
+
 	private String spouseIdNumber;
 
 	private List<String> childNames;
 	private List<String> childIdNumbers;
-	
+
 	public Employee(int yearJoined, Grade grade, int monthJoined, boolean isForeigner) {
 		this.yearJoined = yearJoined;
 		this.grade = grade;
@@ -33,54 +34,60 @@ public class Employee {
 		childNames = new LinkedList<String>();
 		childIdNumbers = new LinkedList<String>();
 	}
-	
-	public void setMonthlySalary(int grade) {	
+
+	public void setMonthlySalary(int grade) {
 		if (grade == 1) {
 			monthlySalary = 3000000;
 			if (isForeigner) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
-		}else if (grade == 2) {
+		} else if (grade == 2) {
 			monthlySalary = 5000000;
 			if (isForeigner) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
-		}else if (grade == 3) {
+		} else if (grade == 3) {
 			monthlySalary = 7000000;
 			if (isForeigner) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
 		}
 	}
-	
-	public void setAnnualDeductible(int deductible) {	
+
+	public void setGrade(Grade grade) {
+		this.grade = grade;
+	}
+
+	public void setAnnualDeductible(int deductible) {
 		this.annualDeductible = deductible;
 	}
-	
-	public void setAdditionalIncome(int income) {	
+
+	public void setAdditionalIncome(int income) {
 		this.otherMonthlyIncome = income;
 	}
-	
+
 	public void setSpouse(String spouseIdNumber) {
 		this.spouseIdNumber = spouseIdNumber;
 	}
-	
+
 	public void addChild(String childName, String childIdNumber) {
 		childNames.add(childName);
 		childIdNumbers.add(childIdNumber);
 	}
-	
+
 	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
+
+		// Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah
+		// bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
-		
+
 		if (date.getYear() == yearJoined) {
 			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
+		} else {
 			monthWorkingInYear = 12;
 		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
+
+		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible,
+				spouseIdNumber.equals(""), childIdNumbers.size());
 	}
 }
